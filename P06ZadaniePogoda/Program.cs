71608190e1b7b270526157c2e41360cb41509260
tomasz <csharp@406-00>
2026-07-01 10:58:27 +0200

    q

--- P06ZadaniePogoda/Program.cs
+++ P06ZadaniePogoda/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,18 @@ namespace P06ZadaniePogoda
     {
         static void Main(string[] args)
         {
+            string url = $"https://wttr.in/{"warszawa"}?format=%t";
+
+            using (WebClient client = new WebClient())
+            {
+                string odpowiedz = client.DownloadString(url);
+                // odpowiedź wygląda np. tak: "+18°C"
+                string liczba = odpowiedz.Replace("+", "").Replace("-", "-")
+                                         .Replace("°C", "").Trim();
+                double tempCelsjusz = double.Parse(liczba);
+
+                
+            }
         }
     }
 }
