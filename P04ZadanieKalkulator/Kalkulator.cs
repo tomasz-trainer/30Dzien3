12124ad1daa108abd50d5bbba863aac372e4098a
tomasz <csharp@406-00>
2026-07-01 10:06:51 +0200

    q

---
+++ P04ZadanieKalkulator/Kalkulator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P04ZadanieKalkulator
+{
+    internal class Kalkulator
+    {
+        public double WykonajDzialanie(int a, int b, char znak)
+        {
+            return wykonajDzialanie(a, b, znak);
+        }
+
+        private double wykonajDzialanie(int a, int b, char znak)
+        {
+            double wynik;
+            if (znak == '+')
+                wynik = a + b;
+            else if (znak == '-')
+                wynik = a - b;
+            else if (znak == '*')
+                wynik = a * b;
+            else if (znak == '/')
+            {
+                if (b == 0)
+                    return Double.NaN;
+                wynik = Convert.ToDouble(a) / b;
+            }
+            else
+            { 
+                throw new ArgumentException("Niepoprawny znak działania");
+            }
+            return wynik;
+        }
+
+
+        public double WykonajDzialanie(string rownanie)
+        {
+            // rozdzielenie rownania na skladowe
+            string op = rownanie.Substring(1, 1);
+            int liczba1 = Convert.ToInt32(rownanie.Substring(0, 1));
+            int liczba2 = Convert.ToInt32(rownanie.Substring(2));
+
+            return wykonajDzialanie(liczba1, liczba2, Convert.ToChar(op));
+        }
+
+    }
+}
