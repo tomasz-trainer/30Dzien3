aea432e99deac6ed3550a8f6515b393b52787afa
tomasz <csharp@406-00>
2026-07-01 15:29:19 +0200

    q

--- P11ZadanieZawodnicy/Program.cs
+++ P11ZadanieZawodnicy/Program.cs
@@ -10,6 +10,15 @@ namespace P11ZadanieZawodnicy.txt
     {
         static void Main(string[] args)
         {
+            ManagerZawodnikow managerZawodnikow = new ManagerZawodnikow();
+            Zawodnik[] zawodnicy = managerZawodnikow
+                .WczytajZawodnikow("C:\\Users\\csharp\\source\\repos\\Dzien3\\P11ZadanieZawodnicy\\zawodnicy.txt");
+
+
+            foreach (var z in zawodnicy)
+            {
+                Console.WriteLine(z.Imie + " " + z.Nazwisko + " " + z.Kraj);
+            }
         }
     }
 }
