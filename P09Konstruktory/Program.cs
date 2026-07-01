31fd4a19d7101daf6a7aefa50d1056648adf6a13
tomasz <csharp@406-00>
2026-07-01 14:01:19 +0200

    q

---
+++ P09Konstruktory/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P09Konstruktory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kalkulator k = new Kalkulator("normalny");
+            //k.Tryb = "normalny";
+
+            string wynik = k.Policz(5, 3);
+        }
+    }
+}
