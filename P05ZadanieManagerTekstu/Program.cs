7100c7ce0777773b7723796660c8bde9b85a3ebf
tomasz <csharp@406-00>
2026-07-01 10:54:36 +0200

    q

--- P05ZadanieManagerTekstu/Program.cs
+++ P05ZadanieManagerTekstu/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using P05ZadanieManagerTekstu.Exceptions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,37 @@ namespace P05ZadanieManagerTekstu
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Podaj zdanie");
+            string zdanie = Console.ReadLine();
+
+            ManagerTekstu mt = new ManagerTekstu();
+
+            try
+            {
+                string pierwszyNajdluzszy = mt.ZnajdzNajdluzszyWyraz(zdanie);
+                Console.WriteLine($"Pierwszy najdluzszy wyraz: {pierwszyNajdluzszy}");
+
+                string[] wyrazy = mt.ZnajdzWszystkieNajdluzszeWyrazy(zdanie);
+                Console.WriteLine($"Wszystkie najdłuższe wyrazy: {string.Join(",", wyrazy)}");
+
+            }
+            catch (ZbytKroktkieZdanieException)
+            {
+                Console.WriteLine("Zbyt krótkie zdanie");
+            }
+            catch (ArgumentException ex)
+            {
+                // zamiast wyswietlac uzytkownikowi powinniśmy logować błąd do pliku logów,
+                // ale na potrzeby tego zadania wyświetlamy komunikat
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Bład przetwarzania tekstu");
+            }
+            Console.ReadKey();
+
         }
+
     }
 }
